b347ff10f680b2c8e561e24e752ec0ad52108cee
Michael Hinrichs <michaelghinrichs@gmail.com>
2018-05-19 07:50:40 -0700

    Add index from module extractor

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
- const path = require('path');
- const fsPromise = require('fs-promise');
- const { readdir, stat } = fsPromise;
+const dt = require('./src/dependency-tree.js')
+const {
+  bootstrapClient,
+  exportToDestination,
+  getTests,
+  processPackageJson,
+} = require('./src/migrator.js')
+const {
+  map,
+  concat,
+  flatten,
+  uniq,
+} = require('ramda')
+const { debug, filterValid, sort } = require('./src/lib.js')
+const {
+  resolverConfig,
+  migratorConfig,
+} = require('./config.js')
 
- async function rreaddir (dir, allFiles = []) {
-   const files = (await readdir(dir)).map(f => path.join(dir, f))
-   allFiles.push(...files)
-   await Promise.all(files.map(async f => (
-     (await stat(f)).isDirectory() && rreaddir(f, allFiles)
-   )))
-   return allFiles
- }
+// configure what we do when we find a dependency.
+// resolve in this context means constructing the correct path so that we can copy it later
+const getDependencies = dt(resolverConfig)
 
- rreaddir(path.join(process.cwd(), process.argv[2])).then(x => console.log(x))
+// crawl for dependencies
+const dependencies = getDependencies(migratorConfig.entryPoints)
+  .then(deps => deps.concat(filterValid(getTests(deps))))
+  .then(concat(migratorConfig.entryPoints))
+  .then(concat(migratorConfig.extraFiles))
+  .then(sort)
+
+if (migratorConfig.debug) {
+  // Log dependencies on Dry runs
+  dependencies
+    .then(map(debug))
+    .then(x => debug(x.length))
+} else {
+  // NEW FILES
+  // Generate extra files from Templates
+
+  // OLD FILES
+  // Copy Files
+  dependencies
+    .then(
+      exportToDestination(
+        migratorConfig.rootDir,
+        migratorConfig.destinationDir
+      )
+    ).then( ()  => {
+      bootstrapClient(
+        `${migratorConfig.rootDir}/src`,
+        migratorConfig.entryPoints
+      ).then(concat([
+        './templates/src/index.js',
+        './templates/webpack.config.js',
+        './templates/webpack.dev.config.js',
+        './templates/.babelrc',
+        './templates/scripts/componentUsage.js',
+        './templates/scripts/single-test.sh',
+      ])).then(
+        exportToDestination(
+          './templates/',
+          migratorConfig.destinationDir
+        )
+      ).then(() => processPackageJson(migratorConfig))
+    })
+}
