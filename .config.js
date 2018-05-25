4aa1d3f27a6cd42146541a8899e01d7d50f13f4c
Michael Hinrichs <michaelghinrichs@gmail.com>
2018-05-25 12:58:53 -0700

    Modify sample config file

--- .config.js
+++ .config.js
@@ -1,9 +1,9 @@
 // Example .config.js file
 module.exports = {
   project: process.cwd(),
-  sourceDir: `${process.cwd()}/testDir`,
+  sourceDir: `${process.cwd()}/exampleDir`,
   entryPoints: [
-    `${process.cwd()}/testDir/root.js`,
+    `${process.cwd()}/exampleDir/root.js`,
   ],
   alternatePaths: ['src','src/_shared'],
 }
